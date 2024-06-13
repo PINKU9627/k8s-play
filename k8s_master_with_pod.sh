7f0c2aee9e850024a7ca46b4ece5ea89fbb2a104
PINKU9627 <142759419+PINKU9627@users.noreply.github.com>
2024-06-13 02:30:22 -0400

    Create k8s_master_with_pod.sh

---
+++ k8s_master_with_pod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+kubeadm init --apiserver-sdvertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/application/nginx-app.yaml
